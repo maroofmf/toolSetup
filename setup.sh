a784702a1f99f1525508dfae557f3742aa9c256c
maroofmf <maroofmf@usc.edu>
2017-05-15 17:59:10 -0700

    Updated setup files to add opencv for python3 and changed tf_reqs

--- setup.sh
+++ setup.sh
@@ -1,11 +1,24 @@
 # Author: Maroof
 # Description: Shell file to install my macos dependencies and config
 
+#----------------------------------------------------------------------------------------------------------------------#
 # MACROS:
-REQ_BREW = requirements_brew.txt
+REQ_BREW=requirements_brew.txt
 
+#---------------------------------------------------------------------------------------------------------------------i#
+# Package: XCODE
+xcodePath=$(xcode-select -p)
+sudo xcodebuild -license accept
+echo "[Error message]:Please download XCODE from app store"
+#----------------------------------------------------------------------------------------------------------------------#
 # Package: git account
+git config --global user.name maroofmf
+git config --global user.email maroofmf@usc.edu
+sudo git config --system user.name maroofmf
+sudo git config --system user.email maroofmf@usc.edu
+echo "Git config complete"
 
+#----------------------------------------------------------------------------------------------------------------------#
 # Package: Install brew and its dependancies
 
 # Install Homebrew
@@ -21,24 +34,33 @@ cat $REQ_BREW | xargs brew install
 
 # Check if path is set to homebrew first
 
-
-
+#----------------------------------------------------------------------------------------------------------------------#
 # Package: VIM
-# Install awesome vim
-# Install vundle
-# Install vundle dependencies: https://github.com/JamshedVesuna/vim-markdown-preview
-# Install grip
 
+# Install awesome vim
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cat vimrc.txt > ~/.vimrc
+# Do:
+#>> vim
+#:PluginInstall
+#----------------------------------------------------------------------------------------------------------------------#
 # Package: Opencv
 # Install opencv and link it with python3
+brew install --force opencv3 --with-contrib --with-python3 --without-python --with-tbb --c++11
+echo /usr/local/opt/opencv3/lib/python3.6/site-packages >> /usr/local/lib/python3.6/site-packages/opencv3.pth
 
+#----------------------------------------------------------------------------------------------------------------------#
 # Package: Bash
 # Replace bashrc from your git
+cat bash_profile.txt > ~/.bash_profile
 
+#----------------------------------------------------------------------------------------------------------------------#
 # Package: Docker
 
-
-# Package: XCode
 
 # Package: Essentials
 #-> Google Chrome
@@ -47,4 +69,10 @@ cat $REQ_BREW | xargs brew install
 #-> Microsoft office
 
 # Package: virtual envs
-
+pip3 install virtualenv
+cd
+cd Desktop
+mkdir virtualenv
+cd virtualenv
+virtualenv tensorflow
+source $TF
